650875ce90bd9ac6ce91dc4ee382ddec6957bc60
Ragnsan Finn Valla <70635181+ragnsan@users.noreply.github.com>
2024-01-07 17:51:56 +0100

    change favicon and update contact email

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const SPOTS_AVAILABLE = 200;
 export const SPOTS_LEFT = 12;
 
-export const CONTACT_EMAIL = "jk@everfit.io";
+export const CONTACT_EMAIL = "hei@myluck.no";
 
 export const PAYMENT_LINK = "https://buy.stripe.com/5kA03oaHl9T24MgeUU";
 
