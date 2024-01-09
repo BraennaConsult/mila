e7b2238c700d164599c03f9cebaa785685c855b9
Ragnsan Finn Valla <70635181+ragnsan@users.noreply.github.com>
2024-01-09 09:41:42 +0100

    add klarna to priority list page

--- app/prioriteringsliste/page.tsx
+++ app/prioriteringsliste/page.tsx
@@ -106,9 +106,17 @@ export default function Page() {
               >
                 Bli med på maraton
               </Button>
-              <span className="text-center text-mid-grey text-md">
-                kr 1 883 per mnd i 3 mnd
-              </span>
+              <div className="flex justify-center text-mid-grey items-center space-x-0.5">
+                <span className="text-center text-mid-grey text-md">
+                  kr 1 883 per mnd i 3 mnd med
+                </span>
+                <Image
+                  src="/klarna.svg"
+                  width={64}
+                  height={64}
+                  alt="Klarna logo"
+                />
+              </div>
             </div>
             <Progress className="mx-auto" />
           </Container>
