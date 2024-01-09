5d915e2584c8b0b856e4a7d90d6404014d64fa9a
Ragnsan Finn Valla <70635181+ragnsan@users.noreply.github.com>
2024-01-09 08:36:49 +0100

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,13 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import "./globals.css";
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://myluck.no"
+    : "http://localhost:3000";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: "Myluck by Mila",
   description: "Gjennomfør treningsmaraton sammen med Mila på Myluck-appen",
 };
