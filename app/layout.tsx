c72342c4637549b808ff0da06e785e00846cb15f
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-04-02 21:39:45 +0200

    feat: revenue tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-white ">
       <head>
-        <PlausibleProvider domain="myluck.no" />
+        <PlausibleProvider domain="myluck.no" revenue />
       </head>
       <body className={hindVadodara.className}>
         {/* <Header /> */}
