505e78d8d277552385e08976f451b3089e04a093
Ragnsan Finn Valla <ragnsan@skaidigital.com>
2024-05-20 14:20:24 +0200

    fix: build errors

--- app/hidden-link/page.tsx
+++ app/hidden-link/page.tsx
@@ -31,69 +31,63 @@ export default function ProductPage({ searchParams }: Props) {
     : false;
 
   return (
-    <>
-      {/* <Header /> */}
-      <main className="bg-[#F7F6F6]">
-        <>
-          <div className="flex justify-center py-12">
-            {/* <Image
-              src={"/my-luck-logo.svg"}
-              alt="myLuck"
-              width={98}
-              height={52}
-            /> */}
-            <Link href="/">
-              <Logo className={`cursor-pointer w-20 "text-black"`} />
-            </Link>
-          </div>
-          <SecretHeaderLimitedOffer countdownTime={countDownTime} />
+    // <>
+    //   <main className="bg-[#F7F6F6]">
+    //     <>
+    //       <div className="flex justify-center py-12">
+    //         <Link href="/">
+    //           <Logo className={`cursor-pointer w-20 "text-black"`} />
+    //         </Link>
+    //       </div>
+    //       <SecretHeaderLimitedOffer countdownTime={countDownTime} />
 
-          {!isLinkExpired && (
-            <>
-              <BeforeAndAfterComponent />
+    //       {!isLinkExpired && (
+    //         <>
+    //           <BeforeAndAfterComponent />
 
-              <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
-                Velg pakken din for <br /> å komme i gang
-              </h3>
-              <p className="text-center text-black/50 text-[14px] mt-6">
-                Maratonet starter 8. April 2024
-              </p>
-              <ProductOptions className="lg:py-10" />
-              <IncludedInPlans />
-              <BenefitsCard />
-              <ProductReview />
-              <MyPath />
-              <h3 className="text-center font-serif text-[40px] leading-[40px] text-black lg:text-2xl">
-                Klar for å få <br /> drømmekroppen din?
-              </h3>
-              <ProductOptions className="lg:pt-10 lg:pb-32" />
-              <Guarantee />
-              <CTA
-                className="pt-0 md:pt-0 lg:pt-0"
-                withTitle={false}
-                disabled={!hasOption}
-                trackingType="checkout_button"
-              />
-              <GoToCheckoutButton show={hasOption} option={option} />
-            </>
-          )}
-          {isLinkExpired && (
-            <>
-              <div className="flex justify-center py-12">
-                <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
-                  Beklager, denne linken er utløpt
-                </h3>
-              </div>
-              <CTA
-                className="pt-0 md:pt-0 lg:pt-0"
-                withTitle={false}
-                disabled={!hasOption}
-                trackingType="checkout_button"
-              />
-            </>
-          )}
-        </>
-      </main>
-    </>
+    //           <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
+    //             Velg pakken din for <br /> å komme i gang
+    //           </h3>
+    //           <p className="text-center text-black/50 text-[14px] mt-6">
+    //             Maratonet starter 8. April 2024
+    //           </p>
+    //           <ProductOptions className="lg:py-10" />
+    //           <IncludedInPlans />
+    //           <BenefitsCard />
+    //           <ProductReview />
+    //           <MyPath />
+    //           <h3 className="text-center font-serif text-[40px] leading-[40px] text-black lg:text-2xl">
+    //             Klar for å få <br /> drømmekroppen din?
+    //           </h3>
+    //           <ProductOptions className="lg:pt-10 lg:pb-32" />
+    //           <Guarantee />
+    //           <CTA
+    //             className="pt-0 md:pt-0 lg:pt-0"
+    //             withTitle={false}
+    //             disabled={!hasOption}
+    //             trackingType="checkout_button"
+    //           />
+    //           <GoToCheckoutButton show={hasOption} option={option} />
+    //         </>
+    //       )}
+    //       {isLinkExpired && (
+    //         <>
+    //           <div className="flex justify-center py-12">
+    //             <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
+    //               Beklager, denne linken er utløpt
+    //             </h3>
+    //           </div>
+    //           <CTA
+    //             className="pt-0 md:pt-0 lg:pt-0"
+    //             withTitle={false}
+    //             disabled={!hasOption}
+    //             trackingType="checkout_button"
+    //           />
+    //         </>
+    //       )}
+    //     </>
+    //   </main>
+    // </>
+    <></>
   );
 }
--- app/private-invite-link/page.tsx
+++ app/private-invite-link/page.tsx
@@ -17,44 +17,45 @@ export default function ProductPage({ searchParams }: Props) {
   const hasOption = searchParams?.option ? true : false;
 
   return (
-    <>
-      <main className="bg-[#F7F6F6]">
-        <>
-          <WhyMyLuckNoCTA />
-          <>
-            <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
-              Velg pakken din for <br /> å komme i gang
-            </h3>
-            <p className="text-center text-black/50 text-[14px] mt-6">
-              Maratonet starter 8. April 2024
-            </p>
-            <ProductOptions className="lg:py-10" />
-            <CTA
-              className="pt-0 md:pt-0 lg:pt-0"
-              withTitle={false}
-              disabled={!hasOption}
-              trackingType="checkout_button"
-            />
-            <IncludedInPlans />
-            <WhatYouGet />
-            <BenefitsCard />
-            <ProductReview />
-            <MyPath />
-            <h3 className="text-center font-serif text-[40px] leading-[40px] text-black lg:text-2xl">
-              Klar for å få <br /> drømmekroppen din?
-            </h3>
-            <ProductOptions className="lg:pt-10 lg:pb-32" />
-            <Guarantee />
-            <CTA
-              className="pt-0 md:pt-0 lg:pt-0"
-              withTitle={false}
-              disabled={!hasOption}
-              trackingType="checkout_button"
-            />
-            <GoToCheckoutButton show={hasOption} option={option} />
-          </>
-        </>
-      </main>
-    </>
+    // <>
+    //   <main className="bg-[#F7F6F6]">
+    //     <>
+    //       <WhyMyLuckNoCTA />
+    //       <>
+    //         <h3 className="text-center font-serif text-[28px] leading-[24px] text-black lg:text-3xl">
+    //           Velg pakken din for <br /> å komme i gang
+    //         </h3>
+    //         <p className="text-center text-black/50 text-[14px] mt-6">
+    //           Maratonet starter 8. April 2024
+    //         </p>
+    //         <ProductOptions className="lg:py-10" />
+    //         <CTA
+    //           className="pt-0 md:pt-0 lg:pt-0"
+    //           withTitle={false}
+    //           disabled={!hasOption}
+    //           trackingType="checkout_button"
+    //         />
+    //         <IncludedInPlans />
+    //         <WhatYouGet />
+    //         <BenefitsCard />
+    //         <ProductReview />
+    //         <MyPath />
+    //         <h3 className="text-center font-serif text-[40px] leading-[40px] text-black lg:text-2xl">
+    //           Klar for å få <br /> drømmekroppen din?
+    //         </h3>
+    //         <ProductOptions className="lg:pt-10 lg:pb-32" />
+    //         <Guarantee />
+    //         <CTA
+    //           className="pt-0 md:pt-0 lg:pt-0"
+    //           withTitle={false}
+    //           disabled={!hasOption}
+    //           trackingType="checkout_button"
+    //         />
+    //         <GoToCheckoutButton show={hasOption} option={option} />
+    //       </>
+    //     </>
+    //   </main>
+    // </>
+    <></>
   );
 }
