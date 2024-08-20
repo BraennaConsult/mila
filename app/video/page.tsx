b3072cf31b901faa0f8bbcdba7cf84e6ae5da5eb
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2024-08-20 12:28:30 +0200

    Update page.tsx

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -6,11 +6,11 @@ export default function VideoPage() {
         <>
             <Header black />
             <main className="bg-[#F7F6F6] flex flex-col items-center justify-center min-h-screen pt-[80px] px-8 pb-12">
-                <div className="w-full max-w-4xl aspect-w-16 aspect-h-9">
+                <div className="w-full aspect-w-16 aspect-h-9">
                     <iframe
                         src="https://player.vimeo.com/video/1000365906"
-                        width="640"
-                        height="360"
+                        width="100%"
+                        height="100%"
                         frameBorder="0"
                         allow="autoplay; fullscreen; picture-in-picture"
                         allowFullScreen
