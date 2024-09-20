d5f0a84a2db379cd868191e024ff99e09921718a
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2024-09-20 12:49:36 +0200

    FinAI test

---
+++ app/chat/page.tsx
@@ -0,0 +1,9 @@
+const ChatPage = () => {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Snakk med FinAI ved å trykke på snakkeboblen</h1>
+      </div>
+    );
+  };
+  
+  export default ChatPage;
