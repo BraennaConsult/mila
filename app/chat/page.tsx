ab68cac1e765d7c8047da10eda0ba6fbf554092b
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2024-09-30 19:38:55 +0100

    test

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,24 @@
+// "use client";
+
+// import { useEffect } from 'react';
+// import Intercom from '@intercom/messenger-js-sdk';
+
+// const ChatPage = () => {
+//   useEffect(() => {
+//     Intercom({
+//       app_id: 'buqv5sev'
+//     });
+//   }, []);
+
+//   return (
+//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+//       <h1>Snakk med FinAI ved å trykke på snakkeboblen</h1>
+//     </div>
+//   );
+// };
+
+// export default ChatPage;
+
 "use client";
 
 import { useEffect } from 'react';
@@ -5,9 +26,18 @@ import Intercom from '@intercom/messenger-js-sdk';
 
 const ChatPage = () => {
   useEffect(() => {
+    
     Intercom({
       app_id: 'buqv5sev'
     });
+
+    
+    window.Intercom('showNewMessage');
+
+    
+    return () => {
+      window.Intercom('shutdown');
+    };
   }, []);
 
   return (
